9a077b6246c35236d444e31b099ddc90b01edf27
notmvson <mason5kong@yahoo.com>
2024-01-25 17:43:48 -0800

    added js

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    
+
+    let newFood = function (pName, pCalories) { //food object template
+        this.foodName = pName;
+        this.foodCalories = pCalories;
+    }
+
+    let foodList = []; //array
+    let totalCaloriesInput = document.getElementById("totalCalories");
+
+    document.getElementById("submitButton").addEventListener("click", function() { //submit button click event
+        let foodName = document.getElementById("foodName").value;
+        let foodCalories = document.getElementById("calorieNumber").value;
+
+        if (foodName && foodCalories) {
+            let newFoodItem = new newFood(foodName, foodCalories);
+            foodList.push(newFoodItem); //adds food item to array
+
+            console.log("New Item Added:", newFoodItem); //updates content of array in console
+        } else {
+            alert("Please enter both food name and calories");
+        }
+    })
+
+
+
+
 })
