882ec5ba6e0a3045407efd2739584edbeb4e1929
cricht-bc <c.richter@bellevuecollege.edu>
2024-01-28 03:42:12 -0800

    better int checker, with error message (partially broken)

--- main.js
+++ main.js
@@ -2,30 +2,32 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     let newFood = function (pName, pCalories) { //food object template
         this.foodName = pName;
-        this.foodCalories = pCalories;
+        this.foodCalories = parseInt(pCalories);
     }
 
     let foodList = []; //array
-    let totalCaloriesInput = parseInt(document.getElementById("totalCalories"));
+    let totalCaloriesInput = 0;//int
 
     document.getElementById("submitButton").addEventListener("click", function() { //submit button click event
         let foodName = document.getElementById("foodName").value;
         let foodCalories = document.getElementById("calorieNumber").value;
 
-        if (foodName && foodCalories) {
+        if (parseInt(foodCalories) = NaN) { //check if valid input
+            alert("Please enter a valid integer for calories input");
+        } else if (foodName && foodCalories) {
             let newFoodItem = new newFood(foodName, foodCalories);
             foodList.push(newFoodItem); //adds food item to array
-
             console.log("New Item Added:", newFoodItem); //updates content of array in console
-            totalCaloriesInput += parseInt(foodCalories);
-            document.getElementById("totalCalories") = totalCaloriesInput;
+            console.log(foodCalories + " Calories Will Be Added To Current Total Of " + totalCaloriesInput + " Calories");//shows current total calories in console
+            totalCaloriesInput += parseInt(foodCalories); // adds int of foodCalories to current total
+            document.getElementById("totalCalories").value = totalCaloriesInput; //pushes total to document
         } else {
             alert("Please enter both food name and calories");
         }
     })
 
-    document.getElementById("resetButton").addEventListener("click", function(){
-        document.getElementById("foodName").value = "";
+    document.getElementById("resetButton").addEventListener("click", function(){ //fully clearing everything, then reload
+        document.getElementById("foodName").value = ""; 
         document.getElementById("calorieNumber").value = "";
         document.getElementById("totalCalories").value = "";
         foodList = [];
